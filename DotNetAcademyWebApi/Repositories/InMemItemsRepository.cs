2bc424c5ad521e3323188c17c8e43acbcdd09aa7
kbuczek <kbuczek@vectorsoftware.pl>
2022-12-25 18:28:26 +0100

    git ignore test

--- DotNetAcademyWebApi/Repositories/InMemItemsRepository.cs
+++ DotNetAcademyWebApi/Repositories/InMemItemsRepository.cs
@@ -11,7 +11,7 @@ namespace DotNetAcademyWebApi.Repositories
             new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
         };
 
-        public IEnumerable<Item> GetItems() //IEnumerable - git ignore
+        public IEnumerable<Item> GetItems() //IEnumerable - git ignore test
         {
             return items;
         }
